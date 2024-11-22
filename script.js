df7d5777dcf3467b034d8bb21f93f8c27cf403f5
Sangeetha Vaddepalli <140165326+SangeethaVaddepalli@users.noreply.github.com>
2024-11-22 21:19:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+function makeBubble(){
+    var clutter = "";
+
+for(var i = 1 ; i <=688 ; i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
+
+
+}
+
+var timer = 60;
+function runTimer(){
+    var timerint = setInterval(function(){
+        if (timer > 0) {
+            timer--;
+        document.querySelector("#timerval").textContent = timer;
+        }
+        else {
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>Game Over</h1>`;
+        }
+        
+        },1000);
+}
+
+var hitrn = 0;
+function getNewHit() {
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+ 
+var score = 0;
+function increaseScore(){
+    score +=10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#pbtm").addEventListener("click",function(dets){
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+
+    }
+});
+
+
+runTimer();
+makeBubble();
+getNewHit();
+increaseScore();
+increaseScore();
+increaseScore();
